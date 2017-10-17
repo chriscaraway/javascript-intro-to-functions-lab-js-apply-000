1bb489d55c1a664367870da28d8873bbc1d335ac
Christopher Caraway <chrisccaraway@gmail.com>
2017-10-17 05:29:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-	var lowercase = "hello";
-	if (string === lowercase){
-      return "I can/'t hear you!"
+	if (string === string.toLowerCase)){
+		return "I can't hear you!"
+	}
 }
